d769fe2875ca510602ef0ae4a7b838bbdbf14e5b
haidangvhbui <haidangvhbui@gmail.com>
2026-09-18 22:10:00 +0700

    Nop bai 1,2,3

---
+++ Bai2Chuong4(Winform)/Bai2Chuong4(Winform)/KHGuiTK.cs
@@ -0,0 +1,62 @@
+﻿namespace Bai2Chuong4_Winform_
+{
+    internal class KHGuiTK
+    {
+        string ma, ten, diaChi, ngayGui;
+        int thoiGG;
+        long soTG;
+        bool loai;
+
+        public KHGuiTK(string ma, string ten, string diaChi, string ngayGui, int thoiGG, long soTG, bool loai)
+        {
+            this.ma = ma;
+            this.ten = ten;
+            this.diaChi = diaChi;
+            this.ngayGui = ngayGui;
+            this.thoiGG = thoiGG;
+            this.soTG = soTG;
+            this.loai = loai;
+        }
+        public double tinhLai()
+        {
+            long TG = soTG;
+            int TGG = thoiGG;
+            bool loai = this.loai;
+            double lai, tyle = 0;
+            if (loai == false)
+            {
+                if (TGG == 1)
+                {
+                    tyle = 0.06;
+                }
+                else
+                    if (TGG == 3)
+                    {
+                        tyle = 0.07;
+                    }
+                    else if (TGG == 6)
+                    {
+                        tyle = 0.08;
+                    }
+                    else
+                    {
+                        tyle = 0.09;
+                    }
+            }
+            if (loai == true)
+            {
+                tyle += 0.01;
+            }
+            lai = (TG * tyle) / 12;
+            return lai;
+        }
+        public string toString()
+        {
+            string loaigui = "";
+            if (loai == false) loaigui = " loai thuong";
+            else loaigui = " phat loc";
+            return ma + " | " + ten + " | " + diaChi + " | " + ngayGui + " | " + thoiGG.ToString() + " thang|" + soTG.ToString() + " | " + loaigui + " | " + tinhLai();
+
+        }
+    }
+}
---
+++ bai3Chuong4(winform)/bai3Chuong4(winform)/Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace bai3Chuong4_winform_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            txtGiaDuThuyen.ReadOnly = true;
+            txtTien.ReadOnly = true;
+            cmbChonDoUong.Items.Add("Coca cola");
+            cmbChonDoUong.Items.Add("Pepsi");
+            cmbChonDoUong.Items.Add("Seven up");
+
+
+            for (int i = 1; i <= 10; i++)
+            {
+                cmbSoLuong.Items.Add(i.ToString());
+            }
+        }
+        private void btnThoat_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Bạn có muốn thoát không", "Thông báo", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+            {
+                this.Close();
+            }
+        }
+
+        private void btnThemMoi_Click(object sender, EventArgs e)
+        {
+            txtHoTen.Text = " ";
+            rdoCaNgay.Checked = false;
+            rdoNuaNgay.Checked = false;
+            txtGiaDuThuyen.Text = " ";
+            cmbChonDoUong.SelectedIndex = -1;
+            cmbSoLuong.SelectedIndex = -1;
+            txtTien.Text = " ";
+            txtHoTen.Focus();
+
+        }
+
+        private void cmbChonDoUong_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            tinhTienDoUong();
+        }
+
+        private void txtTien_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        public void tinhTienDoUong()
+        {
+            if (cmbChonDoUong.SelectedIndex == -1 || cmbSoLuong.SelectedIndex == -1)
+            {
+                return;
+            }
+            double donGia = 0;
+            if (cmbChonDoUong.SelectedIndex == 0)
+            {
+                donGia = 0.5;
+            }
+            else if (cmbChonDoUong.SelectedIndex == 1)
+            {
+                donGia = 0.8;
+            }
+            else if (cmbChonDoUong.SelectedIndex == 2)
+            {
+                donGia = 1.0;
+            }
+
+            int soLuong = int.Parse(cmbSoLuong.SelectedItem.ToString());
+            double tongTienPhaiTra = donGia * soLuong;
+
+            txtTien.Text = tongTienPhaiTra.ToString();
+        }
+
+        private void cmbSoLuong_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            tinhTienDoUong();
+        }
+
+        private void rdoCaNgay_CheckedChanged(object sender, EventArgs e)
+        {
+            if (rdoCaNgay.Checked)
+            {
+                txtGiaDuThuyen.Text = "200";
+            }
+        }
+
+        private void rdoNuaNgay_CheckedChanged(object sender, EventArgs e)
+        {
+            if (rdoNuaNgay.Checked)
+            {
+                txtGiaDuThuyen.Text = "100";
+            }
+        }
+
+        private void btnThemVaoDS_Click(object sender, EventArgs e)
+        {
+            if (txtHoTen.Text == " " || (rdoCaNgay.Checked == false && rdoNuaNgay.Checked == false))
+            {
+                MessageBox.Show("Phải điền đủ thông tin!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                txtHoTen.Focus();
+                return;
+            }
+            string loaiTour = rdoCaNgay.Checked ? "Cả ngày" : "Nửa ngày";
+            double giaDuThuyen = double.Parse(txtGiaDuThuyen.Text);
+            double tienDoUong = 0;
+            if (txtTien.Text.Trim() != "")
+            {
+                tienDoUong = Convert.ToDouble(txtTien.Text);
+            }
+            double tongTien = giaDuThuyen + tienDoUong;
+
+            string hienThi = $"{txtHoTen.Text.Trim()} | {loaiTour} | {giaDuThuyen} $| Đồ uống {tienDoUong} $| Tổng {tongTien}";
+            listBox1.Items.Add(hienThi);
+        }
+    }
+}
